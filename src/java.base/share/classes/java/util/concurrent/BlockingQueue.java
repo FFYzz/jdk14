137cab413ec7baaed2b764ba78b1c27db97349b6
FFYzz <cryptochen95@gmail.com>
2020-07-21 09:59:41 +0800

    BlockingQueue 接口添加注释

--- src/java.base/share/classes/java/util/concurrent/BlockingQueue.java
+++ src/java.base/share/classes/java/util/concurrent/BlockingQueue.java
@@ -42,6 +42,11 @@ import java.util.Queue;
  * A {@link Queue} that additionally supports operations that wait for
  * the queue to become non-empty when retrieving an element, and wait
  * for space to become available in the queue when storing an element.
+ * <p>
+ * 1. 抛异常
+ * 2. 返回一个特殊值，可能是 null 也可能是 false，取决于具体的操作
+ * 3. 阻塞当前线程直到操作能够成功
+ * 4. 带超时时间的获取，超时之后放弃
  *
  * <p>{@code BlockingQueue} methods come in four forms, with different ways
  * of handling operations that cannot be satisfied immediately, but may be
@@ -83,18 +88,27 @@ import java.util.Queue;
  *    <td style="font-style: italic">not applicable</td>
  *  </tr>
  * </table>
+ * <p>
+ * 接口定义规范 Blocking 中不允许存入 null 值
+ * null 的返回值一般用作于 poll 操作为空的返回值
  *
  * <p>A {@code BlockingQueue} does not accept {@code null} elements.
  * Implementations throw {@code NullPointerException} on attempts
  * to {@code add}, {@code put} or {@code offer} a {@code null}.  A
  * {@code null} is used as a sentinel value to indicate failure of
  * {@code poll} operations.
+ * <p>
+ * BlockingQueue 可能是有界的。
  *
  * <p>A {@code BlockingQueue} may be capacity bounded. At any given
  * time it may have a {@code remainingCapacity} beyond which no
  * additional elements can be {@code put} without blocking.
  * A {@code BlockingQueue} without any intrinsic capacity constraints always
  * reports a remaining capacity of {@code Integer.MAX_VALUE}.
+ * <p>
+ * BlockingQueue 的实现主要是用于生产消费队列，且支持 Collection 接口。
+ * 因为支持 Collection 接口，所以支持移除指定的元素。不像传统的队列只能在队头或者
+ * 队尾操作。但是效率很低。
  *
  * <p>{@code BlockingQueue} implementations are designed to be used
  * primarily for producer-consumer queues, but additionally support
@@ -103,6 +117,9 @@ import java.util.Queue;
  * {@code remove(x)}. However, such operations are in general
  * <em>not</em> performed very efficiently, and are intended for only
  * occasional use, such as when a queued message is cancelled.
+ * <p>
+ * BlockingQueue 是线程安全的，内部实现使用了锁或者其他的并发控制方法。
+ * 打的集合相关的操作没有使用锁，所以在操作失败的时候建议抛出异常。
  *
  * <p>{@code BlockingQueue} implementations are thread-safe.  All
  * queuing methods achieve their effects atomically using internal
@@ -172,12 +189,23 @@ import java.util.Queue;
  * <a href="{@docRoot}/java.base/java/util/package-summary.html#CollectionsFramework">
  * Java Collections Framework</a>.
  *
- * @since 1.5
- * @author Doug Lea
  * @param <E> the type of elements held in this queue
+ * @author Doug Lea
+ * @since 1.5
+ */
+
+/**
+ * add/remove
+ * put/take
+ * offer/poll
+ *
+ * @param <E>
  */
 public interface BlockingQueue<E> extends Queue<E> {
     /**
+     * 成功加入进队列返回 true
+     * 如果队满则 抛出异常
+     * <p>
      * Inserts the specified element into this queue if it is possible to do
      * so immediately without violating capacity restrictions, returning
      * {@code true} upon success and throwing an
@@ -187,17 +215,20 @@ public interface BlockingQueue<E> extends Queue<E> {
      *
      * @param e the element to add
      * @return {@code true} (as specified by {@link Collection#add})
-     * @throws IllegalStateException if the element cannot be added at this
-     *         time due to capacity restrictions
-     * @throws ClassCastException if the class of the specified element
-     *         prevents it from being added to this queue
-     * @throws NullPointerException if the specified element is null
+     * @throws IllegalStateException    if the element cannot be added at this
+     *                                  time due to capacity restrictions
+     * @throws ClassCastException       if the class of the specified element
+     *                                  prevents it from being added to this queue
+     * @throws NullPointerException     if the specified element is null
      * @throws IllegalArgumentException if some property of the specified
-     *         element prevents it from being added to this queue
+     *                                  element prevents it from being added to this queue
      */
     boolean add(E e);
 
     /**
+     * 入队成功返回 true
+     * 队满入队失败返回 false
+     * <p>
      * Inserts the specified element into this queue if it is possible to do
      * so immediately without violating capacity restrictions, returning
      * {@code true} upon success and {@code false} if no space is currently
@@ -207,51 +238,60 @@ public interface BlockingQueue<E> extends Queue<E> {
      *
      * @param e the element to add
      * @return {@code true} if the element was added to this queue, else
-     *         {@code false}
-     * @throws ClassCastException if the class of the specified element
-     *         prevents it from being added to this queue
-     * @throws NullPointerException if the specified element is null
+     * {@code false}
+     * @throws ClassCastException       if the class of the specified element
+     *                                  prevents it from being added to this queue
+     * @throws NullPointerException     if the specified element is null
      * @throws IllegalArgumentException if some property of the specified
-     *         element prevents it from being added to this queue
+     *                                  element prevents it from being added to this queue
      */
     boolean offer(E e);
 
     /**
+     * 队满则阻塞等待
+     * 等待期间会响应中断
+     * <p>
      * Inserts the specified element into this queue, waiting if necessary
      * for space to become available.
      *
      * @param e the element to add
-     * @throws InterruptedException if interrupted while waiting
-     * @throws ClassCastException if the class of the specified element
-     *         prevents it from being added to this queue
-     * @throws NullPointerException if the specified element is null
+     * @throws InterruptedException     if interrupted while waiting
+     * @throws ClassCastException       if the class of the specified element
+     *                                  prevents it from being added to this queue
+     * @throws NullPointerException     if the specified element is null
      * @throws IllegalArgumentException if some property of the specified
-     *         element prevents it from being added to this queue
+     *                                  element prevents it from being added to this queue
      */
     void put(E e) throws InterruptedException;
 
     /**
+     * 带等待时间的 offer 方法。
+     * 如果等待时间到了还未入队则返回 false
+     * <p>
      * Inserts the specified element into this queue, waiting up to the
      * specified wait time if necessary for space to become available.
      *
-     * @param e the element to add
+     * @param e       the element to add
      * @param timeout how long to wait before giving up, in units of
-     *        {@code unit}
-     * @param unit a {@code TimeUnit} determining how to interpret the
-     *        {@code timeout} parameter
+     *                {@code unit}
+     * @param unit    a {@code TimeUnit} determining how to interpret the
+     *                {@code timeout} parameter
      * @return {@code true} if successful, or {@code false} if
-     *         the specified waiting time elapses before space is available
-     * @throws InterruptedException if interrupted while waiting
-     * @throws ClassCastException if the class of the specified element
-     *         prevents it from being added to this queue
-     * @throws NullPointerException if the specified element is null
+     * the specified waiting time elapses before space is available
+     * @throws InterruptedException     if interrupted while waiting
+     * @throws ClassCastException       if the class of the specified element
+     *                                  prevents it from being added to this queue
+     * @throws NullPointerException     if the specified element is null
      * @throws IllegalArgumentException if some property of the specified
-     *         element prevents it from being added to this queue
+     *                                  element prevents it from being added to this queue
      */
     boolean offer(E e, long timeout, TimeUnit unit)
-        throws InterruptedException;
+            throws InterruptedException;
 
     /**
+     * 对应 put
+     * 从队列中取元素，若队列为空，则阻塞等待。
+     * <p>
      * Retrieves and removes the head of this queue, waiting if necessary
      * until an element becomes available.
      *
@@ -261,21 +301,27 @@ public interface BlockingQueue<E> extends Queue<E> {
     E take() throws InterruptedException;
 
     /**
+     * 从队列中获取元素，带超时时间，如果指定时间内未获取到，
+     * 则返回 null
+     * <p>
      * Retrieves and removes the head of this queue, waiting up to the
      * specified wait time if necessary for an element to become available.
      *
      * @param timeout how long to wait before giving up, in units of
-     *        {@code unit}
-     * @param unit a {@code TimeUnit} determining how to interpret the
-     *        {@code timeout} parameter
+     *                {@code unit}
+     * @param unit    a {@code TimeUnit} determining how to interpret the
+     *                {@code timeout} parameter
      * @return the head of this queue, or {@code null} if the
-     *         specified waiting time elapses before an element is available
+     * specified waiting time elapses before an element is available
      * @throws InterruptedException if interrupted while waiting
      */
     E poll(long timeout, TimeUnit unit)
-        throws InterruptedException;
+            throws InterruptedException;
 
     /**
+     * 返回队列中剩余的可以存放元素的个数
+     * 如果是无界队列则返回 Integer.MAX_VALUE
+     * <p>
      * Returns the number of additional elements that this queue can ideally
      * (in the absence of memory or resource constraints) accept without
      * blocking, or {@code Integer.MAX_VALUE} if there is no intrinsic
@@ -291,6 +337,10 @@ public interface BlockingQueue<E> extends Queue<E> {
     int remainingCapacity();
 
     /**
+     * 移除指定的元素，如果存在多个，则只移除一个。
+     * 成功返回 true，失败返回 false
+     * 具体实现是从头到尾便利？还是从尾到头遍历？
+     * <p>
      * Removes a single instance of the specified element from this queue,
      * if it is present.  More formally, removes an element {@code e} such
      * that {@code o.equals(e)}, if this queue contains one or more such
@@ -300,30 +350,34 @@ public interface BlockingQueue<E> extends Queue<E> {
      *
      * @param o element to be removed from this queue, if present
      * @return {@code true} if this queue changed as a result of the call
-     * @throws ClassCastException if the class of the specified element
-     *         is incompatible with this queue
-     * (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     * @throws ClassCastException   if the class of the specified element
+     *                              is incompatible with this queue
+     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
      * @throws NullPointerException if the specified element is null
-     * (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
      */
     boolean remove(Object o);
 
     /**
+     * 判断队列中是否存在某个元素
+     * <p>
      * Returns {@code true} if this queue contains the specified element.
      * More formally, returns {@code true} if and only if this queue contains
      * at least one element {@code e} such that {@code o.equals(e)}.
      *
      * @param o object to be checked for containment in this queue
      * @return {@code true} if this queue contains the specified element
-     * @throws ClassCastException if the class of the specified element
-     *         is incompatible with this queue
-     * (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     * @throws ClassCastException   if the class of the specified element
+     *                              is incompatible with this queue
+     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
      * @throws NullPointerException if the specified element is null
-     * (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
      */
     boolean contains(Object o);
 
     /**
+     * 移除队列中的所有元素，并将元素放入到 Collection 中
+     * <p>
      * Removes all available elements from this queue and adds them
      * to the given collection.  This operation may be more
      * efficient than repeatedly polling this queue.  A failure
@@ -338,17 +392,20 @@ public interface BlockingQueue<E> extends Queue<E> {
      * @param c the collection to transfer elements into
      * @return the number of elements transferred
      * @throws UnsupportedOperationException if addition of elements
-     *         is not supported by the specified collection
-     * @throws ClassCastException if the class of an element of this queue
-     *         prevents it from being added to the specified collection
-     * @throws NullPointerException if the specified collection is null
-     * @throws IllegalArgumentException if the specified collection is this
-     *         queue, or some property of an element of this queue prevents
-     *         it from being added to the specified collection
+     *                                       is not supported by the specified collection
+     * @throws ClassCastException            if the class of an element of this queue
+     *                                       prevents it from being added to the specified collection
+     * @throws NullPointerException          if the specified collection is null
+     * @throws IllegalArgumentException      if the specified collection is this
+     *                                       queue, or some property of an element of this queue prevents
+     *                                       it from being added to the specified collection
      */
     int drainTo(Collection<? super E> c);
 
     /**
+     * 从队列中移除最多指定数量的 element ，并将这些元素加入到 Collection 中
+     * 发生异常的处理情况看具体实现
+     * <p>
      * Removes at most the given number of available elements from
      * this queue and adds them to the given collection.  A failure
      * encountered while attempting to add elements to
@@ -359,17 +416,17 @@ public interface BlockingQueue<E> extends Queue<E> {
      * this operation is undefined if the specified collection is
      * modified while the operation is in progress.
      *
-     * @param c the collection to transfer elements into
+     * @param c           the collection to transfer elements into
      * @param maxElements the maximum number of elements to transfer
      * @return the number of elements transferred
      * @throws UnsupportedOperationException if addition of elements
-     *         is not supported by the specified collection
-     * @throws ClassCastException if the class of an element of this queue
-     *         prevents it from being added to the specified collection
-     * @throws NullPointerException if the specified collection is null
-     * @throws IllegalArgumentException if the specified collection is this
-     *         queue, or some property of an element of this queue prevents
-     *         it from being added to the specified collection
+     *                                       is not supported by the specified collection
+     * @throws ClassCastException            if the class of an element of this queue
+     *                                       prevents it from being added to the specified collection
+     * @throws NullPointerException          if the specified collection is null
+     * @throws IllegalArgumentException      if the specified collection is this
+     *                                       queue, or some property of an element of this queue prevents
+     *                                       it from being added to the specified collection
      */
     int drainTo(Collection<? super E> c, int maxElements);
 }
